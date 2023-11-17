18f2ff2edafb2c15c23df15cc6f6267e8304c995
whyest <whyest@gmail.com>
2023-11-17 13:00:27 +0300

    add auth-server logic

--- auth-cli/src/App.jsx
+++ auth-cli/src/App.jsx
@@ -5,31 +5,55 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-	const [loggedIn, setLoggedIn] = useState(false)
-	const [email, setEmail] = useState('')
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState('')
 
-	return (
-		<div className='App'>
-			<BrowserRouter>
-				<Routes>
-					<Route
-						path='/'
-						element={
-							<Home
-								email={email}
-								loggedIn={loggedIn}
-								setLoggedIn={setLoggedIn}
-							/>
-						}
-					/>
-					<Route
-						path='/login'
-						element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</div>
-	)
+  useEffect(() => {
+    // Fetch the user email and token from local storage
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    // If the token/email does not exist, mark the user as logged out
+    if (!user || !user.token) {
+      setLoggedIn(false)
+      return
+    }
+
+    // If the token exists, verify it with the auth server to see if it is valid
+    fetch('http://localhost:3080/verify', {
+      method: 'POST',
+      headers: {
+        'jwt-token': user.token,
+      },
+    })
+      .then((r) => r.json())
+      .then((r) => {
+        setLoggedIn('success' === r.message)
+        setEmail(user.email || '')
+      })
+  }, [])
+
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Home
+                email={email}
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+              />
+            }
+          />
+          <Route
+            path='/login'
+            element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
--- auth-cli/src/Home.jsx
+++ auth-cli/src/Home.jsx
@@ -1,30 +1,42 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const Home = props => {
-	const { loggedIn, email } = props
-	const navigate = useNavigate()
+const Home = (props) => {
+  const { loggedIn, email } = props
+  const navigate = useNavigate()
 
-	const onButtonClick = () => {
-		// You'll update this function later
-	}
+  const onButtonClick = () => {
+    if (loggedIn) {
+      localStorage.removeItem('user')
+      props.setLoggedIn(false)
+    } else {
+      navigate('/login')
+    }
+  }
 
-	return (
-		<div className='mainContainer'>
-			<div className='titleContainer'>
-				<div>Добро пожаловать</div>
-			</div>
-			<div>Главная страница</div>
-			<div className='buttonContainer'>
-				<input
-					className='inputButton'
-					type='button'
-					onClick={onButtonClick}
-					value={loggedIn ? 'Выйти' : 'Войти'}
-				/>
-				{loggedIn ? <div>Your email address is {email}</div> : <div />}
-			</div>
-		</div>
-	)
+  return (
+    <div className='mainContainer'>
+      <div className='titleContainer'>
+        <div>Добро пожаловать</div>
+      </div>
+      <div>Главная страница</div>
+      <div className='buttonContainer'>
+        <input
+          className='inputButton'
+          type='button'
+          onClick={onButtonClick}
+          value={loggedIn ? 'Выйти' : 'Войти'}
+        />
+        {loggedIn ? (
+          <div>
+            Ваш электронный адрес: {''}
+            {email}
+          </div>
+        ) : (
+          <div />
+        )}
+      </div>
+    </div>
+  )
 }
 export default Home
